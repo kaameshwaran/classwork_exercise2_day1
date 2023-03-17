31174dc5d01ff7add11b9836704484099b05cd06
kaamehwaran <727721euec062@skcet.ac.in>
2023-03-17 16:07:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Reference Data types use same memory</h1>
       <button onClick={window["run"]}>Click</button>
       </header>
     </div>
